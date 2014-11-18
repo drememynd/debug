79d1d2586deb2d007384f94b306811a792a168aa
Katrina Wolfe <drememynd@gmail.com>
2014-11-18 10:36:30 -0800

    Update index.php

--- files/index.php
+++ files/index.php
@@ -1,35 +1,6 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="chrome=1">
-    <title>Debug by drememynd</title>
-
-    <link rel="stylesheet" href="stylesheets/styles.css">
-    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="wrapper">
-      <header>
-        <h1>Debug</h1>
-        <p>PHP debug output</p>
-
-        <p class="view"><a href="https://github.com/drememynd/debug">View the Project on GitHub <small>drememynd/debug</small></a></p>
-
-
-        <ul>
-          <li><a href="https://github.com/drememynd/debug/zipball/master">Download <strong>ZIP File</strong></a></li>
-          <li><a href="https://github.com/drememynd/debug/tarball/master">Download <strong>TAR Ball</strong></a></li>
-          <li><a href="https://github.com/drememynd/debug">View On <strong>GitHub</strong></a></li>
-        </ul>
-      </header>
-      <section>
-        <h3>
-<a id="debug" class="anchor" href="#debug" aria-hidden="true"><span class="octicon octicon-link"></span></a>Debug</h3>
+---
+layout: layout
+---
 
 <h1>Files:</h1>
 
@@ -42,13 +13,4 @@
 <li>backtrace.php : backtrace information class</li>
 <li>builder.php : builds the debug string information</li>
 <li>timer.php : keeps track of the time</li>
-      </section>
-      <footer>
-        <p>This project is maintained by <a href="https://github.com/drememynd">drememynd</a></p>
-        <p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
-      </footer>
-    </div>
-    <script src="javascripts/scale.fix.js"></script>
-    
-  </body>
-</html>
+      
